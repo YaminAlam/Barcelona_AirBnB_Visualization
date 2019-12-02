36c86cd664e277f326eab9883bd4df7ff9e2acf6
Yamin Alam <31516813+YaminAlam@users.noreply.github.com>
2019-12-02 14:27:13 -0500

    Add files via upload

---
+++ flask-app-tester/static/js/config.js
@@ -0,0 +1,2 @@
+// API Key
+const API_KEY = "pk.eyJ1IjoieWFtaW5hbGFtIiwiYSI6ImNrM25xb2UxYzF0OHoza251bHNmcHNxZ3IifQ.rr63YszYl6p4X6P3nmqQxw";
---
+++ flask-app-tester/static/js/logic.js
@@ -0,0 +1,15 @@
+
+var myMap = L.map("map-id", {
+  center: [41.3851, 2.1734],
+  zoom: 13
+});
+
+// Adding a tile layer (the background map image) to our map
+var layer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: "pk.eyJ1IjoieWFtaW5hbGFtIiwiYSI6ImNrM25xb2UxYzF0OHoza251bHNmcHNxZ3IifQ.rr63YszYl6p4X6P3nmqQxw"
+}).addTo(myMap);
+
+myMap.addLayer(layer);
